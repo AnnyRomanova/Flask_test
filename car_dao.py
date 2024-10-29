11c28776fa3f766565156e3c790fea1d21e5eda0
AnnyRomanova <anna.romanova0818@gmail.com>
2024-10-29 15:15:51 +0300

    Разбила на еще один модуль

--- car_dao.py
+++ car_dao.py
@@ -1,8 +1,5 @@
 import sqlite3  # база данных
-from car_dao import select_data, add_user_data
 from logging import getLogger, basicConfig, DEBUG  # для логирования
-from flask import Flask, jsonify, request
-
 
 # настраиваем логирование
 logger = getLogger()  # создание своего логера
@@ -15,30 +12,20 @@ with sqlite3.connect('cars.sqlite', check_same_thread=False) as connection:
 logger.info("Подключение к базе данных")
 
 
-app = Flask(__name__)
+#  кладем данные из бд в список
+def select_data():
+    cursor.execute("SELECT * FROM cars")
+    cars_list = cursor.fetchall()
+    logger.info("Получены данные из базы данных")
+    return cars_list
 
 
-@app.route('/cars', methods=['GET'])
-def get_cars():
-    cars = select_data()
-    logger.info(cars)
-    return jsonify(cars)
-
-
-@app.route('/cars', methods=['POST'])
-def post_cars():
-    data = request.get_json()
-    new_car = {
-        "model": data["model"],
-        "color": data["color"],
-        "transmission": data["transmission"],
-        "quantity": data["quantity"]
-    }
-
-    add_user_data(data)
-
-    return 201
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# метод добавляет новые данные в бд
+def add_user_data(data):
+    data_list = []
+    for value in data.values():
+        data_list.append(value)
+    query = " INSERT INTO cars (model, color, transmission, quantity) VALUES(?,?,?,?); "
+    cursor.execute(query, tuple(data_list))
+    connection.commit()
+    logger.debug("Добавлены новые данные в БД")
